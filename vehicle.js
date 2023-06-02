9c0c064e523b2a8e6a2cffffeda746a9e84eaf3d
Gustavo Bozzano <gustavocbozzano@gmail.com>
2023-06-02 08:42:45 -0300

    fix header e substituido save por upsert

--- vehicle.js
+++ vehicle.js
@@ -20,16 +20,17 @@ document.addEventListener("DOMContentLoaded", function () { // sera executado qu
         const parts = dtEntrada.split('-'); // formata padrao BR
         const dtEntradaFormat = `${parts[2]}/${parts[1]}/${parts[0]}`;
         try {
-            new Service(marca, modelo, placa, ano, dtEntradaFormat, orcamento, servicos, dsServico);
-            alert("Serviço cadastrado com êxito");
-            document.getElementById("marca").value = "";
-            document.getElementById("modelo").value = "";
-            document.getElementById("placa").value = "";
-            document.getElementById("ano").value = "";
-            document.getElementById("dtEntrada").value = "";
-            document.getElementById("orcamento").value = "";
-            document.getElementById("servicos").value = "";
-            document.getElementById("dsServico").value = "";
+            const service = new Service(marca, modelo, placa, ano, dtEntradaFormat, orcamento, servicos, dsServico);
+            if (service.upsert()) {
+                document.getElementById("marca").value = "";
+                document.getElementById("modelo").value = "";
+                document.getElementById("placa").value = "";
+                document.getElementById("ano").value = "";
+                document.getElementById("dtEntrada").value = "";
+                document.getElementById("orcamento").value = "";
+                document.getElementById("servicos").value = "";
+                document.getElementById("dsServico").value = "";
+            }
             // falta cliente
         } catch (error) {
             alert(`Erro ao cadastrar veículo: ${error}`);
